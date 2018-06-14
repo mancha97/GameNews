c5264a551a565f6924562d70691b2867b443e9ad
Carlos Valle <carlos.momos@gmail.com>
2018-06-13 23:12:37 -0600

    Adding api components

--- app/src/main/java/com/valle00018316/gamenews/GameNApi/Api.java
+++ app/src/main/java/com/valle00018316/gamenews/GameNApi/Api.java
@@ -1,4 +1,19 @@
 package com.valle00018316.gamenews.GameNApi;
 
+
+
+import retrofit2.Call;
+import retrofit2.http.Field;
+import retrofit2.http.FormUrlEncoded;
+import retrofit2.http.POST;
+
 public interface Api {
+
+    static final String END_POINT = "http://gamenewsuca.herokuapp.com";
+    @FormUrlEncoded
+    @POST("/login")
+    Call<String> login(
+            @Field("user") String username,
+            @Field("password") String password
+    );
 }
--- app/src/main/java/com/valle00018316/gamenews/GameNApi/Deserializer/Token.java
+++ app/src/main/java/com/valle00018316/gamenews/GameNApi/Deserializer/Token.java
@@ -1,4 +1,20 @@
 package com.valle00018316.gamenews.GameNApi.Deserializer;
 
-public class Token {
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+
+public class Token implements JsonDeserializer<String> {
+    @Override
+    public String deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+        JsonObject tokenJson = json.getAsJsonObject();
+        String token = tokenJson.get("token").getAsString();
+
+        return token;
+    }
 }
