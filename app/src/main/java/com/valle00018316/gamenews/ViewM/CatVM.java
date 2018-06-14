4676cf9c528dff7a41c04a443f33d5ef277a64c4
Carlos Valle <carlos.momos@gmail.com>
2018-06-14 01:22:03 -0600

    adding viewmodel for categories

--- app/src/main/java/com/valle00018316/gamenews/ViewM/CatVM.java
+++ app/src/main/java/com/valle00018316/gamenews/ViewM/CatVM.java
@@ -1,4 +1,29 @@
 package com.valle00018316.gamenews.ViewM;
 
-public class CatVM {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.valle00018316.gamenews.Dba.Entidad.Categoria;
+import com.valle00018316.gamenews.Dba.Entidad.Noticia;
+import com.valle00018316.gamenews.Dba.Repo.CategoRepo;
+import com.valle00018316.gamenews.Dba.Repo.NotiRepo;
+
+import java.util.List;
+
+public class CatVM extends AndroidViewModel{
+
+    private CategoRepo mRepository;
+
+
+
+    public CatVM(Application app) {
+        super(app);
+        mRepository = new CategoRepo(app);
+
+    }
+
+    public LiveData<List<Categoria>> getCategories() { return mRepository.getCategories();}
+
+    public void insert(Categoria categoria) { mRepository.insert(categoria); }
 }
