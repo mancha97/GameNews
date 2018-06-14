4c5742f0667c03561a798f717f2eff75b3a11368
Carlos Valle <carlos.momos@gmail.com>
2018-06-14 01:20:38 -0600

    Adding List Game Deserializer

--- app/src/main/java/com/valle00018316/gamenews/GameNApi/Deserializer/ListG.java
+++ app/src/main/java/com/valle00018316/gamenews/GameNApi/Deserializer/ListG.java
@@ -1,4 +1,26 @@
 package com.valle00018316.gamenews.GameNApi.Deserializer;
 
-public class List {
+import com.google.gson.JsonArray;
+import com.google.gson.JsonDeserializationContext;
+import com.google.gson.JsonDeserializer;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParseException;
+
+import java.lang.reflect.Type;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListG implements JsonDeserializer<List<String>> {
+    @Override
+    public List<String> deserialize(JsonElement json, Type typeOfT, JsonDeserializationContext context) throws JsonParseException {
+
+        JsonArray listJson = json.getAsJsonArray();
+        List<String> lista = new ArrayList<>();
+        for(JsonElement x:listJson){
+            lista.add(x.getAsString());
+        }
+
+        return lista;
+    }
 }
