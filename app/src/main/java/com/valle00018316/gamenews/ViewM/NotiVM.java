a8db64389394e064cd681f8e7c7432d534b770a9
Carlos Valle <carlos.momos@gmail.com>
2018-06-13 23:17:37 -0600

    creating drawer components

--- app/src/main/java/com/valle00018316/gamenews/ViewM/NotiVM.java
+++ app/src/main/java/com/valle00018316/gamenews/ViewM/NotiVM.java
@@ -1,4 +1,27 @@
 package com.valle00018316.gamenews.ViewM;
 
-public class NotiVM {
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LiveData;
+
+import com.valle00018316.gamenews.Dba.Entidad.Noticia;
+import com.valle00018316.gamenews.Dba.Repo.NotiRepo;
+
+import java.util.List;
+
+public class NotiVM extends AndroidViewModel {
+
+    private NotiRepo mRepository;
+
+    private LiveData<List<Noticia>> mAllNoti;
+
+    public NotiVM (Application application) {
+        super(application);
+        mRepository = new NotiRepo(application);
+        mAllNoti = mRepository.getAllNoti();
+    }
+
+    LiveData<List<Noticia>> getAllNoti() { return mAllNoti; }
+
+    public void insert(Noticia noticia) { mRepository.insert(noticia); }
 }
