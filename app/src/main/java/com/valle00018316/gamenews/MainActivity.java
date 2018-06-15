2dfface9503c50082e84398ce8e6406a76955858
Carlos Valle <carlos.momos@gmail.com>
2018-06-14 21:16:11 -0600

    beta for shared preferences usage

--- app/src/main/java/com/valle00018316/gamenews/MainActivity.java
+++ app/src/main/java/com/valle00018316/gamenews/MainActivity.java
@@ -1,6 +1,7 @@
 package com.valle00018316.gamenews;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -8,11 +9,18 @@ import android.os.Bundle;
 public class MainActivity extends AppCompatActivity {
 
     private static int SPLASH_TIME_OUT=4000;
+    String access;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        SharedPreferences sharedPreferences=this.getSharedPreferences("logbait",MODE_PRIVATE);
 
+        if(sharedPreferences.contains("logbait")){
+            Intent homeIntent= new Intent(MainActivity.this, HomeActivity.class);
+            startActivity(homeIntent);
+            finish();
+        }
 
         new Handler().postDelayed(new Runnable() {
             @Override
