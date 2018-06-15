53d9fa9dcd2955fc2a534792a56ff9d86892f0ff
Carlos Valle <carlos.momos@gmail.com>
2018-06-14 20:32:39 -0600

    Creating a News Model to insert in the database

--- app/src/main/java/com/valle00018316/gamenews/Models/NoticiaM.java
+++ app/src/main/java/com/valle00018316/gamenews/Models/NoticiaM.java
@@ -1,4 +1,85 @@
 package com.valle00018316.gamenews.Models;
 
 public class NoticiaM {
+
+
+    private String id;
+    private String title, coverImage, description, body,createdDate, game;
+
+    private int isFav;
+
+    public NoticiaM() {
+
+    }
+
+
+
+
+    public String getId() {
+
+        return id;
+    }
+
+    public void setId( String id) {
+        this.id = id;
+    }
+
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getCoverImage() {
+        return coverImage;
+    }
+
+    public void setCoverImage(String coverImage) {
+        this.coverImage = coverImage;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    public String getGame() {
+        return game;
+    }
+
+    public void setGame(String game) {
+        this.game = game;
+    }
+
+    public String getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(String createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public int getIsFav() {
+        return isFav;
+    }
+
+    public void setIsFav(int isFav) {
+        this.isFav = isFav;
+    }
 }
+
+
